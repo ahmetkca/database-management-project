7437b5934a0baadc57259ca24e4609069d4b4f7f
ahmetkca <ahmet.karapinar@ontariotechu.net>
2021-11-15 13:50:15 -0500

    deleted run script because it is renamed

--- run.sh
+++
@@ -1,6 +0,0 @@
-docker-compose down && docker-compose kill
-docker volume rm database-management-project_frontend_volume
-docker volume rm database-management-project_backend_volume
-docker rmi database-management-project_backend
-docker rmi database-management-project_frontend
-docker-compose up -d --build
